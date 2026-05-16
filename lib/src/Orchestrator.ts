454868a0539ceaeb287fb5734540b49510f14d89
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-05-16 02:14:40 +0200

    ensure users database file creation

--- lib/src/Orchestrator.ts
+++ lib/src/Orchestrator.ts
@@ -2,9 +2,14 @@
 
     // natives
     import { join } from "node:path";
+    import { writeFile, mkdir, rm } from "node:fs/promises";
 
     // externals
     import { Orchestrator } from "node-pluginsmanager-plugin";
+
+    // locals
+    import isFile from "./utils/isFile";
+    import isDirectory from "./utils/isDirectory";
 
 // types & interfaces
 
@@ -15,6 +20,8 @@
 
 export default class OrchestratorTemplate extends Orchestrator {
 
+    // constructor
+
     public constructor (data: iOrchestratorOptions) {
 
         super({
@@ -23,6 +30,54 @@ export default class OrchestratorTemplate extends Orchestrator {
             "descriptorFile": join(__dirname, "..", "data", "Descriptor.json"),
             "mediatorFile": join(__dirname, "Mediator.js"),
             "serverFile": join(__dirname, "Server.js")
+        });
+
+    }
+
+    protected _initWorkSpace (): Promise<void> {
+
+        return this.install();
+
+    }
+
+    public install (): Promise<void> {
+
+        return isDirectory(this._externalResourcesDirectory).then((check: boolean): Promise<string | undefined> => {
+
+            if (check) {
+                return Promise.resolve("");
+            }
+
+            return mkdir(this._externalResourcesDirectory, { "recursive": true });
+
+        }).then((): Promise<void> => {
+
+            const usersFile = join(this._externalResourcesDirectory, "users.json");
+
+            return isFile(usersFile).then((check: boolean): Promise<void> => {
+
+                if (check) {
+                    return Promise.resolve();
+                }
+
+                return writeFile(usersFile, "{}", "utf-8");
+
+            });
+
+        });
+
+    }
+
+    public uninstall (): Promise<void> {
+
+        return isDirectory(this._externalResourcesDirectory).then((check: boolean): Promise<void> | void => {
+
+            if (check) {
+                return Promise.resolve();
+            }
+
+            return rm(this._externalResourcesDirectory, { "recursive": true });
+
         });
 
     }
